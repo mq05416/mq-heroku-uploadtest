4c1989cbe4131e10f6b1fbc1d1c79cad3a93f046
Luan Nguyen <luannv.aptech2015@gmail.com>
2016-11-29 18:48:38 -0800

    v2

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -6,7 +6,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 @Entity
-@Table(name = "USER")
+@Table(name = "USER_TABLE")
 public class User {
 	@Id
 	@Column(name = "USERNAME")
